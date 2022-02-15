64ac4f6fbcefbe2b40d1ee82bde778947c5e2573
唐干宵 <1587900660@qq.com>
2022-02-15 16:03:49 +0800

    build: 打包成静态页面

---
+++ dist/assets/js/6.d402a159.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{459:function(n,t,e){"use strict";e.r(t);var s=e(83),a=Object(s.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"Bar"},[this._v("\n    --------------------------- 这里是 Foo/Bar.vue 组件的内容 ---------------------------\n")])}),[],!1,null,null,null);t.default=a.exports}}]);
