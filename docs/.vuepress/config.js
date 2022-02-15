0b76ddf00de11eaa2f8f186dafbbe1ccc2be663b
唐干宵 <1587900660@qq.com>
2022-02-15 16:00:42 +0800

    feature: 调整为多个侧边栏的导航模式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,16 @@
 // vuepress 的文档地址 https://vuepress.vuejs.org/zh/config/
-const tools = require('./utils')
 
-const randomIcon = ['/tang.ico', '/gan.ico', '/xiao.ico']
+const tools = require('./utils')
 
 module.exports = {
     // 部署站点的基准路径, 默认是 /
     base: '/tgx-blog/',
 
     // 左上角的标题
-    title: 'telectron 的博客',
+    title: 'telectron 的网络日志',
 
     // 网站的 SEO 描述
-    description: '唐干宵的博客，一个普通上班族的博客，telectron 的博客，谭竿霄的博客',
+    description: '唐干宵的网络日志|博客，一个普通上班族的网络日志|博客，telectron 的网络日志|博客，谭竿霄的网络日志|博客',
 
     // 需要被注入到 <head> 标签的内容
     head: [
@@ -23,7 +22,7 @@ module.exports = {
                 rel: 'shortcut icon',
                 // 路径指向 .vuepress/public
                 // href: '/xiao.ico'
-                href: tools.getRandomElement(randomIcon)
+                href: tools.getRandomElement(['/tang.ico', '/gan.ico', '/xiao.ico']) // 随机取一个 favicon
             }
         ]
     ],
@@ -36,7 +35,18 @@ module.exports = {
         lineNumbers: true
     },
 
-    plugins: ['@vuepress/last-updated'],
+    // plugins: [
+    //     '@vuepress/last-updated',
+    //     {
+    //         transformer: (timestamp, lang) => {
+    //             // 不要忘了安装 moment
+    //             console.log('--------------');
+    //             console.log(timestamp, lang);
+    //             console.log('--------------');
+    //             return '99999999'
+    //         }
+    //     }
+    // ],
 
     // vuepress 默认主题 的配置
     themeConfig: {
@@ -54,40 +64,46 @@ module.exports = {
                     // 5. 如果 link 为 '/test/test-001.md', vuepress 会去 docs/test 文件夹中查找 test-001.md
                     // { text: 'test', link: '/test/test-001' },
                     // 为了可读性, 我们将采取 方案4 来管理文章链接
-                    { text: 'vue2', link: '/frontend/vue2/personal-blog' }
-                    // { text: 'vue3', link: '/frontend/vue3/' }
+                    { text: 'vue2', link: '/frontend/vue2/personal-blog' },
+                    { text: 'git', link: '/frontend/git/git-command' }
                 ]
             }
         ],
         // 侧边栏菜单
-        sidebar: [
-            {
-                // 设置一级标题(子标题会从.md文档中自动提取)
-                title: '使用 vuepress 搭建个人博客',
-                // 对应的文章路径(同 nav 里面的 link, 采取方案4)
-                path: '/frontend/vue2/personal-blog'
-            },
-            {
-                title: '资源整合',
-                path: '/frontend/vue2/bk-open-source'
-            },
-            {
-                title: 'Git 命令收集',
-                path: '/frontend/vue2/git-command'
-            },
-            {
-                title: '旧项目 Eslint 升级体验',
-                path: '/frontend/vue2/eslint-upgrade'
-            }
-        ],
+        sidebar: {
+            '/frontend/vue2': [
+                {
+                    // 设置一级标题 (vuepress 会从 .md 文档中自动提取 二级标题)
+                    title: '使用 vuepress 搭建个人博客',
+                    // 对应的文章路径 (同 nav 里面的 link, 采取方案4)
+                    path: '/frontend/vue2/personal-blog'
+                },
+                {
+                    title: '资源整合',
+                    path: '/frontend/vue2/source-integration'
+                },
+                {
+                    title: '旧项目 Eslint 升级体验',
+                    path: '/frontend/vue2/eslint-upgrade'
+                }
+            ],
+            '/frontend/git': [
+                {
+                    title: 'Git 命令收集',
+                    path: '/frontend/git/git-command'
+                }
+            ]
+        },
         // 标题深度，2 表示提取 h2 和 h3 标题
         sidebarDepth: 2,
 
         // 是否启用页面滚动效果
         smoothScroll: true,
+        // 是否在滚动页面时实时更新 URL 值
+        activeHeaderLinks: false,
 
         // 最后更新时间
-        lastUpdated: '最后更新时间',
+        lastUpdated: '上次更新',
 
         // 是否开启页面底部的 下一篇 链接
         nextLinks: true,
--- docs/README.md
+++ docs/README.md
@@ -7,10 +7,10 @@ actionText: 开始阅读 →
 actionLink: /frontend/vue2/personal-blog
 features:
     - title: 简洁
-      details: 多则杂，杂则乱，乱则无章。有用的文章，必定是解决某个具体问题的一段文字。
+      details: 多则杂，杂则乱，乱则无章。有用的文章，必须要解决某个具体问题。
     - title: 实践
-      details: 自己动手才能发现真正的实施问题，试图解决它，然后写一小段总结。
+      details: 自己动手实施才能发现具体的问题，试图解决它，然后写一段总结，以后就按这个总结的套路来。
     - title: 总结
-      details: 有必要收集一些自己解决过的问题，形成记忆图，以便日后快速查找，腾出更多打游戏的时间。
+      details: 有必要收集一些自己解决过的问题，形成记忆图，以便快速查找，腾出更多打游戏的时间。
 footer: Copyright © 2022-present telectron
 ---
