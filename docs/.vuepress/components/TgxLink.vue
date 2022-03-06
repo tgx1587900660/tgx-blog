9d22562031545e006b11536492ffc0f30b284ffd
唐干宵 <1587900660@qq.com>
2022-03-06 17:54:42 +0800

    bugfix: 调整了a链接组件的封装，兼容了站内站外两种链接格式

--- docs/.vuepress/components/TgxLink.vue
+++ docs/.vuepress/components/TgxLink.vue
@@ -1,6 +1,6 @@
 <!-- 自定义的 a 链接组件 -->
 <template>
-  <a class="tgx-link" :title="href" target="_blank" :href="href">
+  <a class="tgx-link" :title="localHref" target="_blank" :href="localHref">
     <slot name="default">链接名称</slot>
   </a>
 </template>
@@ -12,6 +12,15 @@ export default {
     href: {
       type: String,
       default: 'https://gitee.com/tgx-1587900660'
+    }
+  },
+  computed: {
+    // 在线地址, 原样返回; 内部链接, 补上前缀
+    localHref() {
+      if (this.href.includes('http://') || this.href.includes('https://')) {
+        return this.href
+      }
+      return '/tgx-blog' + this.href
     }
   }
 }
--- docs/backend/node/node.md
+++ docs/backend/node/node.md
@@ -10,7 +10,7 @@
 
 - 浏览器：是 js 的 **前端运行环境**
 - Node：是 js 的 **后端运行环境**
-- <tgx-link href="/frontend/browser/browser">点击查看浏览器运行环境</tgx-link>
+- <tgx-link href="/frontend/browser/browser">这篇文章</tgx-link> 有 浏览器运行环境 相关知识
 
 :::
 
@@ -212,5 +212,5 @@ server.listen(80, () => {
 ## 6. querystring 字符串处理模块
 ::: tip 说明
 querystring 模块是 Node.js 官方提供的、用来处理字符串格式数据的模块。
-<tgx-link href="/tgx-blog/backend/node/express">这篇文章</tgx-link> 的【第三方 中间件】中用到了 querystring 模块
+<tgx-link href="/backend/node/express">这篇文章</tgx-link> 的【第三方 中间件】中用到了 querystring 模块
 :::
