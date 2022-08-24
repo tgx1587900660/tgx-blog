011ff3b184cf99e84bfb89f9435b794867c5e1d9
唐干宵 <1587900660@qq.com>
2022-08-24 14:51:47 +0800

    minior: 修改文件名

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,41 +1,69 @@
+// 这个文件是要配合 vscode 的插件 prettier 使用的，要确保编辑器安装了 prettier插件
 module.exports = {
-  // 字符串使用单引号
+  /**
+   * 字符串使用单引号
+   */
   singleQuote: true,
-  // 每行末尾自动添加分号
-  semi: true,
-  // tab缩进大小,默认为2
+
+  /**
+   * 对象最后一个元素自动添加 拖尾逗号
+   * none: 不添加
+   */
+  trailingComma: 'none',
+
+  /**
+   * 每行末尾自动添加分号
+   */
+  semi: false,
+
+  /**
+   * tab缩进大小,默认为2
+   */
   tabWidth: 2,
-  // 使用tab缩进，默认false
+
+  /**
+   * 使用tab缩进，默认false
+   */
   useTabs: false,
-  // 对象中打印空格 默认true
-  // true: { foo: bar }
-  // false: {foo: bar}
+
+  /**
+   * 对象中打印空格 默认true
+   * true: { foo: bar }
+   * false: {foo: bar}
+   */
   bracketSpacing: true,
-  // 箭头函数参数括号 默认avoid 可选 avoid| always
-  // avoid 能省略括号的时候就省略 例如x => x
-  // always 总是有括号
+
+  /**
+   * 箭头函数参数括号 默认avoid 可选 avoid| always
+   * avoid 能省略括号的时候就省略 例如x => x
+   * always 总是有括号
+   */
   arrowParens: 'avoid',
-  // 换行长度，默认80
+
+  /**
+   * 换行长度，默认80
+   */
   printWidth: 80,
 
-  // 设置为true时,将多行JSX元素的 > 放在最后一行的末尾，而不是单独放在下一行
-  /* 
-    <button
+  /**
+   * 将多行JSX元素的 > 放在最后一行的末尾，而不是单独放在下一行
+   * 例子：
+   * 设置为true时：
+   * <button
        className="prettier-class"
        id="prettier-id"
        onClick={this.handleClick}>
        Click Here
-    </button> 
-     */
-  // 设置为false时
-  /*
-    <button
-        className="prettier-class"
-        id="prettier-id"
-        onClick={this.handleClick}
-    >
-        Click Here
     </button>
-     */
+   * 
+   * 设置为false时
+   * <button
+       className="prettier-class"
+       id="prettier-id"
+       onClick={this.handleClick}
+    >
+       Click Here
+    </button>
+   */
   jsxBracketSameLine: true
 }
---
+++ test/js/01.test-prettier.js
@@ -0,0 +1,8 @@
+const a =    {0: 'asd',
+}
+let b = 2;
+
+
+// 预期格式化之后 会变成：
+// const a = { 0: 'asd' }
+// let b = 2
